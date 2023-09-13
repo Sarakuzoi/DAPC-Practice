7d98384ecc6f2c7032a9f00092e906b92fd00b7a
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-09-14 00:06:21 +0200

    Updated output compiler c++ version to 11

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROG_NAME=$1
 
-output=$(g++ $PROG_NAME 2>&1)
+output=$(g++ -std=c++11 $PROG_NAME 2>&1)
 if [[ $? != 0 ]]; then
     # There was an error, display the error in $output
     echo -e "Error:\n$output"
